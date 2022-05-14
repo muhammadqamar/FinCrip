b4b40e46976d18316d10b7d4e416a9f9c6dac419
muhammadqamar <muhammadqamar111@gmail.com>
2022-05-14 16:42:29 +0500

    added road mao

--- src/components/homePage/roadMap.js
+++ src/components/homePage/roadMap.js
@@ -1,19 +1,64 @@
-import React from "react";
-import { Image } from "react-bootstrap";
-import style from "./style//roadMap.module.scss";
+import React from 'react';
+import { Image } from 'react-bootstrap';
+import style from './style/roadMap.module.scss';
 
 const RoadMap = () => {
   return (
     <div className={style.roadMap_main}>
       <div className={style.roadMap_section}>
+        <img src="/croppedline.png" alt="" className={style.liner} />
+        <img src="/Vector.png" alt="" className={style.liner2} />
+
         <h5 className={style.roadMap_heading}>Road Map</h5>
-        <Image
-          className={style.roadMap_Image}
-          alt="roadMap_Image"
-          src="/icons/teamLine.svg"
-          width={1440}
-          height={0}
-        />
+        <div className={`${style.box1} ${style.first}`}>
+          <div className={style.box} />
+          <div className={style.flexdiv}>
+            <h5>Q1 2022</h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at
+              odio non lectus accumsan pretium ut at erat.
+            </p>
+          </div>
+        </div>
+        <div className={style.rocket}>
+          <div className={style.box} />
+          <img src="./Rocket.png" alt="" />
+          <img src="./rs.png" alt="" className={style.mobiler} />
+
+          <h6>WE ARE HERE!</h6>
+        </div>
+        <div className={`${style.box1} ${style.second}`}>
+          <div className={style.box} />
+          <div className={style.flexdiv}>
+            <h5>Q1 2022</h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at
+              odio non lectus accumsan pretium ut at erat.
+            </p>
+          </div>
+        </div>
+        <div className={`${style.box1} ${style.third}`}>
+          <div className={style.box} />
+          <div className={style.flexdiv}>
+            <div className={style.flexdiv}>
+              <h5>Q1 2022</h5>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+                at odio non lectus accumsan pretium ut at erat.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className={`${style.box1} ${style.four}`}>
+          <div className={style.box} />
+          <div className={style.flexdiv}>
+            <h5>Q1 2022</h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at
+              odio non lectus accumsan pretium ut at erat.
+            </p>
+          </div>
+        </div>
       </div>
       <div></div>
     </div>
